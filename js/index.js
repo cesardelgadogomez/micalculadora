70c9e4cd644aedba9aae908ac072b6d14ee78cb9
cesardelgadogomez <cesadelgado2110@gmail.com>
2025-04-03 14:48:46 -0500

    corrigiendo el JS para añadir funcionalidad de keydown a través del teclado.

--- js/index.js
+++ js/index.js
@@ -3,20 +3,45 @@ let buttons = document.querySelectorAll("button");
 
 let string = "";
 let array = Array.from(buttons);
+
+function handleInput(value) {
+  if (value === '=') {
+    string = eval(string);
+    inputBox.value = string;
+  } else if (value === 'AC') {
+    string = "";
+    inputBox.value = string;
+  } else if (value === 'DEL') {
+    string = string.substring(0, string.length - 1);
+    inputBox.value = string;
+  } else {
+    string += value;
+    inputBox.value = string;
+  }
+}
+
 array.forEach(button => {
   button.addEventListener("click", (e) => {
-    if(e.target.innerHTML == '=') {
-      string = eval(string);
-      inputBox.value = string;
-    } else if (e.target.innerHTML == 'AC') {
-      string = "";
-      inputBox.value = string;
-    } else if (e.target.innerHTML == "DEL") {
-      string = string.substring(0, string.length - 1);
-      inputBox.value = string;
-    } else {
-      string += e.target.innerHTML;
-      inputBox.value = string;
-    }
-  })
-})
+    handleInput(e.target.innerHTML);
+  });
+});
+
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+
+  if (/^[0-9]$/.test(key)) {
+    handleInput(key);
+  } else if (key === '.') {
+    handleInput(key);
+  }  else if (key === '+' || key === '-' || key === '*' || key === '/') {
+    handleInput(key);
+  } else if (key === '%') {
+    handleInput(key);
+  } else if (key === 'Enter') {
+    handleInput('=');
+  } else if (key === 'Backspace') {
+    handleInput('DEL');
+  } else if (key === 'Escape') {
+    handleInput('AC');
+  }
+});
